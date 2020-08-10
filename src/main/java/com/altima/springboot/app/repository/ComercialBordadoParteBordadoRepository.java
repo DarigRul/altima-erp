71290cc1fc22b23b3af58eac41962806ecc83ab3
LeonardoTiDev <60356628+LeonardoTiDev@users.noreply.github.com>
2020-08-10 11:17:30 -0500

    cambios

---
+++ src/main/java/com/altima/springboot/app/repository/ComercialBordadoParteBordadoRepository.java
@@ -0,0 +1,9 @@
+package com.altima.springboot.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altima.springboot.app.models.entity.ComercialBordadoParteBordado;
+
+public interface ComercialBordadoParteBordadoRepository extends CrudRepository<ComercialBordadoParteBordado, Long> {
+
+}
