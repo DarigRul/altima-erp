2b2ea0a5054d8ac2fc73ee03bdb41f371c20682d
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-08-12 10:37:09 -0500

    Add files via upload

---
+++ src/main/java/com/altima/springboot/app/repository/ComprasProveedorContactoRepository.java
@@ -0,0 +1,11 @@
+package com.altima.springboot.app.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altima.springboot.app.models.entity.ComprasProveedorContacto;
+
+public interface ComprasProveedorContactoRepository extends CrudRepository<ComprasProveedorContacto, Long> {
+
+
+}
