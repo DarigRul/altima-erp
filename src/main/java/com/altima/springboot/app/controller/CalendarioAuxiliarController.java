224d4e6afd1e8cb3c5df05aac572ed22778ef0aa
kamuhdez <kamudoce@gmail.com>
2020-08-17 16:58:06 -0500

    Calendario Auxiliar

--- src/main/java/com/altima/springboot/app/controller/CalendarioAuxiliarController.java
+++ src/main/java/com/altima/springboot/app/controller/CalendarioAuxiliarController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CalendarioGeneralController {
-    @GetMapping("/calendario")
+public class CalendarioAuxiliarController {
+    @GetMapping("/calendario-auxiliar")
     public String CalendarioGeneral()
     {
-        return "calendario";
+        return "calendario-auxiliar";
     }
 }
