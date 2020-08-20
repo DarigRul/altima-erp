f7ff0507e23259ec7d818a41b8f5d6605a990cb4
LeonardoTiDev <60356628+LeonardoTiDev@users.noreply.github.com>
2020-08-20 16:02:06 -0500

    cambios en la pantalla de carga de pedidos : Avisos en monto de precio y descuento de razon social

--- src/main/java/com/altima/springboot/app/models/service/CargaPedidoServiceImpl.java
+++ src/main/java/com/altima/springboot/app/models/service/CargaPedidoServiceImpl.java
@@ -53,21 +53,73 @@ public class CargaPedidoServiceImpl implements ICargaPedidoService {
 	public List<Object[]> CargaPedidoVista(Long iduser) {
 		List<Object[]> re = null;
 		if (iduser != null) {
-			re = em.createNativeQuery("SELECT\r\n" + "	informacion.id_pedido_informacion,\r\n"
-					+ "	informacion.id_text,\r\n" + "	cliente.nombre,\r\n"
-					+ "	IFNULL( DATE( informacion.fecha_entrega ), 'Por definir' ),\r\n" + "	cliente.id_cliente, informacion.observacion \r\n"
-					+ "FROM\r\n" + "	alt_comercial_pedido_informacion AS informacion,\r\n"
-					+ "	alt_comercial_cliente AS cliente \r\n" + "WHERE\r\n" + "	1 = 1 \r\n"
-					+ "	AND informacion.id_empresa = cliente.id_cliente \r\n" + "	AND informacion.id_usuario = "
-					+ iduser + " \r\n" + "ORDER BY\r\n" + "	informacion.fecha_creacion DESC").getResultList();
+			re = em.createNativeQuery("SELECT\r\n" + 
+					"	informacion.id_pedido_informacion,\r\n" + 
+					"	informacion.id_text,\r\n" + 
+					"	cliente.nombre,\r\n" + 
+					"	IFNULL( DATE( informacion.fecha_entrega ), 'Por definir' ),\r\n" + 
+					"	cliente.id_cliente,\r\n" + 
+					"	informacion.observacion,\r\n" + 
+					"	(\r\n" + 
+					"	SELECT\r\n" + 
+					"		IFNULL( sum( coorpre.monto_adicional ), 0 ) \r\n" + 
+					"	FROM\r\n" + 
+					"		alt_comercial_coordinado_prenda coorpre\r\n" + 
+					"		INNER JOIN alt_comercial_coordinado coor ON coorpre.id_coordinado = coor.id_coordinado\r\n" + 
+					"		INNER JOIN alt_comercial_pedido_informacion pedinf ON coor.id_pedido = pedinf.id_pedido_informacion \r\n" + 
+					"	WHERE\r\n" + 
+					"		pedinf.id_pedido_informacion = informacion.id_pedido_informacion \r\n" + 
+					"	) AS cargoPrecio,\r\n" + 
+					"	(\r\n" + 
+					"	SELECT\r\n" + 
+					"		IFNULL( sum( mtrz.descuento ), 0 ) \r\n" + 
+					"	FROM\r\n" + 
+					"		alt_comercial_total_razon_social mtrz\r\n" + 
+					"		INNER JOIN alt_comercial_pedido_informacion pedinf2 ON mtrz.id_pedido = pedinf2.id_pedido_informacion \r\n" + 
+					"	WHERE\r\n" + 
+					"		pedinf2.id_pedido_informacion = informacion.id_pedido_informacion \r\n" + 
+					"	) AS desceunto \r\n" + 
+					"FROM\r\n" + 
+					"	alt_comercial_pedido_informacion informacion\r\n" + 
+					"	INNER JOIN alt_comercial_cliente cliente ON informacion.id_empresa = cliente.id_cliente AND informacion.id_usuario = "+ iduser +" \r\n" + 
+					"GROUP BY\r\n" + 
+					"	informacion.id_pedido_informacion \r\n" + 
+					"ORDER BY\r\n" + 
+					"	informacion.fecha_creacion DESC").getResultList();
 		} else {
-			re = em.createNativeQuery("SELECT\r\n" + "	informacion.id_pedido_informacion,\r\n"
-					+ "	informacion.id_text,\r\n" + "	cliente.nombre,\r\n"
-					+ "	IFNULL( DATE( informacion.fecha_entrega ), 'Por definir' ),\r\n" + "	cliente.id_cliente, informacion.observacion \r\n"
-					+ "FROM\r\n" + "	alt_comercial_pedido_informacion AS informacion,\r\n"
-					+ "	alt_comercial_cliente AS cliente \r\n" + "WHERE\r\n" + "	1 = 1 \r\n"
-					+ "	AND informacion.id_empresa = cliente.id_cliente \r\n" + "ORDER BY\r\n"
-					+ "	informacion.fecha_creacion DESC").getResultList();
+			re = em.createNativeQuery("SELECT\r\n" + 
+					"	informacion.id_pedido_informacion,\r\n" + 
+					"	informacion.id_text,\r\n" + 
+					"	cliente.nombre,\r\n" + 
+					"	IFNULL( DATE( informacion.fecha_entrega ), 'Por definir' ),\r\n" + 
+					"	cliente.id_cliente,\r\n" + 
+					"	informacion.observacion,\r\n" + 
+					"	(\r\n" + 
+					"	SELECT\r\n" + 
+					"		IFNULL( sum( coorpre.monto_adicional ), 0 ) \r\n" + 
+					"	FROM\r\n" + 
+					"		alt_comercial_coordinado_prenda coorpre\r\n" + 
+					"		INNER JOIN alt_comercial_coordinado coor ON coorpre.id_coordinado = coor.id_coordinado\r\n" + 
+					"		INNER JOIN alt_comercial_pedido_informacion pedinf ON coor.id_pedido = pedinf.id_pedido_informacion \r\n" + 
+					"	WHERE\r\n" + 
+					"		pedinf.id_pedido_informacion = informacion.id_pedido_informacion \r\n" + 
+					"	) AS cargoPrecio,\r\n" + 
+					"	(\r\n" + 
+					"	SELECT\r\n" + 
+					"		IFNULL( sum( mtrz.descuento ), 0 ) \r\n" + 
+					"	FROM\r\n" + 
+					"		alt_comercial_total_razon_social mtrz\r\n" + 
+					"		INNER JOIN alt_comercial_pedido_informacion pedinf2 ON mtrz.id_pedido = pedinf2.id_pedido_informacion \r\n" + 
+					"	WHERE\r\n" + 
+					"		pedinf2.id_pedido_informacion = informacion.id_pedido_informacion \r\n" + 
+					"	) AS desceunto \r\n" + 
+					"FROM\r\n" + 
+					"	alt_comercial_pedido_informacion informacion\r\n" + 
+					"	INNER JOIN alt_comercial_cliente cliente ON informacion.id_empresa = cliente.id_cliente \r\n" + 
+					"GROUP BY\r\n" + 
+					"	informacion.id_pedido_informacion \r\n" + 
+					"ORDER BY\r\n" + 
+					"	informacion.fecha_creacion DESC").getResultList();
 		}
 
 		return re;
