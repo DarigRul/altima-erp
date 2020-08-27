982ae05cab531d9e09c220a15426566a14a42071
warriorx72 <39719764+warriorx72@users.noreply.github.com>
2020-08-27 09:42:35 -0500

    ticket aux

--- src/main/java/com/altima/springboot/app/dto/ComercialTicketDTO.java
+++ src/main/java/com/altima/springboot/app/dto/ComercialTicketDTO.java
@@ -7,12 +7,15 @@ public class ComercialTicketDTO {
     private String title;
     private String start;
     private String end;
+    private Long id;
 
     public ComercialTicketDTO(ComercialTicket ticket) {
         super();
+
         this.title = ticket.getDescripcion();
         this.start = ticket.getFechaInicio();
         this.end = ticket.getFechaFin();
+        this.id = ticket.getIdTicket();
 
     }
 
@@ -40,6 +43,13 @@ public class ComercialTicketDTO {
         this.end = end;
     }
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     
 }
