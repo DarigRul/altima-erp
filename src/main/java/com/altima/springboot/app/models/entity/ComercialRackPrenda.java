947a87bf651158f2a7caf2cfc3c94c913456e102
DTI <DTI@DESKTOP-AVK6DCT>
2020-09-10 18:13:44 -0500

    Nueva Entidad

---
+++ src/main/java/com/altima/springboot/app/models/entity/ComercialRackPrenda.java
@@ -0,0 +1,164 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_comercial_rack_prenda")
+public class ComercialRackPrenda implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id_rack_prenda")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GenericGenerator(name="native",strategy="native")
+	private Long idRackPrenda;
+	
+	@Column(name="id_prenda")
+	private Long idPrenda;
+	
+	@Column(name="id_vendedor")
+	private Long idVendedor;
+	
+	@Column(name="id_empresa")
+	private Long idEmpresa;
+	
+	@Column(name="encargado")
+	private String encargado;
+	
+	@Column(name="precio")
+	private String precio;
+	
+	@Column(name="cantidad")
+	private Long cantidad;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="actualizado_por")
+	private String actualizadoPor;
+	
+	@Column(name="fecha_creacion")
+	private String fechaCreacion;
+	
+	@Column(name="ultima_fecha_modificacion")
+	private String ultimaFechaModificacion;
+	
+	@Column(name="estatus")
+	private String estatus;
+
+	public Long getIdRackPrenda() {
+		return idRackPrenda;
+	}
+
+	public void setIdRackPrenda(Long idRackPrenda) {
+		this.idRackPrenda = idRackPrenda;
+	}
+
+	public Long getIdPrenda() {
+		return idPrenda;
+	}
+
+	public void setIdPrenda(Long idPrenda) {
+		this.idPrenda = idPrenda;
+	}
+
+	public Long getIdVendedor() {
+		return idVendedor;
+	}
+
+	public void setIdVendedor(Long idVendedor) {
+		this.idVendedor = idVendedor;
+	}
+
+	public Long getIdEmpresa() {
+		return idEmpresa;
+	}
+
+	public void setIdEmpresa(Long idEmpresa) {
+		this.idEmpresa = idEmpresa;
+	}
+
+	public String getEncargado() {
+		return encargado;
+	}
+
+	public void setEncargado(String encargado) {
+		this.encargado = encargado;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public Long getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Long cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+}
