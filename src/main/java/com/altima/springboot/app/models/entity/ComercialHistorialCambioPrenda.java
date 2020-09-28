0c43f0d42b61099771b9fd28521e2330af9ad2a6
DTI <DTI@DESKTOP-AVK6DCT>
2020-09-28 16:10:18 -0500

    Entidad nueva

---
+++ src/main/java/com/altima/springboot/app/models/entity/ComercialHistorialCambioPrenda.java
@@ -0,0 +1,143 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_comercial_historial_cambio_prenda")
+public class ComercialHistorialCambioPrenda implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="id_historial")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long idHistorial;
+	
+	@Column(name="id_concentrado_prenda")
+	private Long idConcentradoPrenda;
+	
+	@Column(name="id_prenda")
+	private Long idPrenda;
+	
+	@Column(name="id_text")
+	private String idText;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="actualizado_por")
+	private String actualizadoPor;
+	
+	@Column(name="fecha_creacion")
+	private String fechaCreacion;
+	
+	@Column(name="ultima_fecha_modificacion")
+	private String ultimaFechaModificacion;
+
+	@Column(name="descripcion_ruta")
+	private String descripcionRuta;
+	
+	@Column(name="estatus")
+	private String Estatus;
+
+	public Long getIdHistorial() {
+		return idHistorial;
+	}
+
+	public void setIdHistorial(Long idHistorial) {
+		this.idHistorial = idHistorial;
+	}
+
+	public Long getIdConcentradoPrenda() {
+		return idConcentradoPrenda;
+	}
+
+	public void setIdConcentradoPrenda(Long idConcentradoPrenda) {
+		this.idConcentradoPrenda = idConcentradoPrenda;
+	}
+
+	public Long getIdPrenda() {
+		return idPrenda;
+	}
+
+	public void setIdPrenda(Long idPrenda) {
+		this.idPrenda = idPrenda;
+	}
+
+	public String getIdText() {
+		return idText;
+	}
+
+	public void setIdText(String idText) {
+		this.idText = idText;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public String getDescripcionRuta() {
+		return descripcionRuta;
+	}
+
+	public void setDescripcionRuta(String descripcionRuta) {
+		this.descripcionRuta = descripcionRuta;
+	}
+
+	public String getEstatus() {
+		return Estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		Estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+	  
+}
