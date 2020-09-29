b5239c71fb22905625276ad1335ddb90a6009d9c
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-09-29 11:00:12 -0500

    Add files via upload

---
+++ src/main/java/com/altima/springboot/app/repository/ComercialAgentesVentaRepository.java
@@ -0,0 +1,9 @@
+package com.altima.springboot.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altima.springboot.app.models.entity.ComercialAgentesVenta;
+
+public interface ComercialAgentesVentaRepository extends CrudRepository<ComercialAgentesVenta, Long>{
+
+}
