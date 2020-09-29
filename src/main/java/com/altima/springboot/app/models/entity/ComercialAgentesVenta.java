82ca90783e6a946446ef914f8f05a01849a70a24
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-09-29 10:58:08 -0500

    Add files via upload

---
+++ src/main/java/com/altima/springboot/app/models/entity/ComercialAgentesVenta.java
@@ -0,0 +1,110 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "alt_comercial_agentes_venta")
+public class ComercialAgentesVenta implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name="id_empleado")
+	private String idEmpleado;
+	
+	@Column(name="foraneo")
+	private String foraneo;
+	
+	@Column(name="licitacion")
+	private String licitacion;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="actualizado_por")
+	private String actualizadoPor;
+	
+	@Column(name="fecha_creacion")
+	private String fechaCreacion;
+	
+	@Column(name="ultima_fecha_modificacion")
+	private String ultimaFechaModificacion;
+	
+	@Column(name="estatus")
+	private String estatus;
+
+	public String getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(String idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public String getForaneo() {
+		return foraneo;
+	}
+
+	public void setForaneo(String foraneo) {
+		this.foraneo = foraneo;
+	}
+
+	public String getLicitacion() {
+		return licitacion;
+	}
+
+	public void setLicitacion(String licitacion) {
+		this.licitacion = licitacion;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
