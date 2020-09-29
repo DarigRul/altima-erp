b34106bd160430db47efac0d99ee04be044f507f
DTI <DTI@DESKTOP-AVK6DCT>
2020-09-28 19:29:45 -0500

    Nueva Entidad

---
+++ src/main/java/com/altima/springboot/app/models/entity/ComercialSolicitudModelo.java
@@ -0,0 +1,186 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_comercial_solicitud_modelo")
+public class ComercialSolicitudModelo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id_solicitud_modelo")
+	@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native",strategy="native")
+	private Long idSolicitudModelo;
+	
+	@Column(name="id_text")
+	private String idText;
+	
+	@Column(name="id_agente")
+	private Long idAgente;
+	
+	@Column(name="id_modelo")
+	private Long idModelo;
+	
+	@Column(name="id_cliente")
+	private Long idCliente;
+	
+	@Column(name="fecha_cita")
+	private String fechaCita;
+	
+	@Column(name="hora_salida_altima")
+	private String horaSalidaAltima;
+	
+	@Column(name="cantidad_modelo_caballero")
+	private Long cantidadModeloCaballero;
+	
+	@Column(name="cantidad_modelo_dama")
+	private Long cantidadModeloDama;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="actualizado_por")
+	private String actualizadoPor;
+	
+	@Column(name="fecha_creacion")
+	private String fechaCreacion;
+	
+	@Column(name="ultima_fecha_actualizacion")
+	private String ultimaFechaModificacion;
+	
+	@Column(name="estatus")
+	private String estatus;
+
+	public Long getIdSolicitudModelo() {
+		return idSolicitudModelo;
+	}
+
+	public void setIdSolicitudModelo(Long idSolicitudModelo) {
+		this.idSolicitudModelo = idSolicitudModelo;
+	}
+
+	public String getIdText() {
+		return idText;
+	}
+
+	public void setIdText(String idText) {
+		this.idText = idText;
+	}
+
+	public Long getIdAgente() {
+		return idAgente;
+	}
+
+	public void setIdAgente(Long idAgente) {
+		this.idAgente = idAgente;
+	}
+
+	public Long getIdModelo() {
+		return idModelo;
+	}
+
+	public void setIdModelo(Long idModelo) {
+		this.idModelo = idModelo;
+	}
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getFechaCita() {
+		return fechaCita;
+	}
+
+	public void setFechaCita(String fechaCita) {
+		this.fechaCita = fechaCita;
+	}
+
+	public String getHoraSalidaAltima() {
+		return horaSalidaAltima;
+	}
+
+	public void setHoraSalidaAltima(String horaSalidaAltima) {
+		this.horaSalidaAltima = horaSalidaAltima;
+	}
+
+	public Long getCantidadModeloCaballero() {
+		return cantidadModeloCaballero;
+	}
+
+	public void setCantidadModeloCaballero(Long cantidadModeloCaballero) {
+		this.cantidadModeloCaballero = cantidadModeloCaballero;
+	}
+
+	public Long getCantidadModeloDama() {
+		return cantidadModeloDama;
+	}
+
+	public void setCantidadModeloDama(Long cantidadModeloDama) {
+		this.cantidadModeloDama = cantidadModeloDama;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+}
