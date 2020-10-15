aee88c650cd65ee9c578b5918cbea0d7228c634c
dtico <dtico@DESKTOP-GBT4PGS>
2020-10-15 14:22:06 -0500

    arregloooos

--- src/main/resources/static/dist/js/tablaInventarioAMP.js
+++ src/main/resources/static/dist/js/tablaInventarioAMP.js
@@ -26,9 +26,9 @@
                 });
             },
             "columnDefs": [
-                { "width": "10%", "targets": 0 },
+                { "width": "5%", "targets": 0 },
                 { "width": "25%", "targets": 1},
-                { "width": "15%", "targets": 2 },
+                { "width": "20%", "targets": 2 },
                 { "width": "10%", "targets": 3 },
                 { "width": "10%", "targets": 4 },
                 { "width": "10%", "targets": 5 },
