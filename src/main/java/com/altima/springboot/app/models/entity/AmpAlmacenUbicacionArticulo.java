9edd1c78619b33e52f2fe1b92d377dd82b48e53f
DTI <DTI@DESKTOP-AVK6DCT>
2020-10-22 18:17:08 -0500

    Entidad

---
+++ src/main/java/com/altima/springboot/app/models/entity/AmpAlmacenUbicacionArticulo.java
@@ -0,0 +1,143 @@
+package com.altima.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "alt_amp_almacen_ubicacion_articulo")
+public class AmpAlmacenUbicacionArticulo implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id_conteo")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GenericGenerator(name="native",strategy="native")
+	private Long idConteo;
+	
+	@Column(name="numero_conteo")
+	private Long numeroConteo;
+	
+	@Column(name="id_ubicacion")
+	private Long idUbicacion;
+	
+	@Column(name="id_articulo")
+	private Long idArticulo;
+	
+	@Column(name="cantidad")
+	private Long cantidad;
+	
+	@Column(name="creado_por")
+	private String creadoPor;
+	
+	@Column(name="actualizado_por")
+	private String actualizadoPor;
+	
+	@Column(name="fecha_creacion")
+	private String fechaCreacion;
+	
+	@Column(name="ultima_fecha_modificacion")
+	private String ultimaFechaModificacion;
+	
+	@Column(name="estatus")
+	private String estatus;
+
+	public Long getIdConteo() {
+		return idConteo;
+	}
+
+	public void setIdConteo(Long idConteo) {
+		this.idConteo = idConteo;
+	}
+
+	public Long getNumeroConteo() {
+		return numeroConteo;
+	}
+
+	public void setNumeroConteo(Long numeroConteo) {
+		this.numeroConteo = numeroConteo;
+	}
+
+	public Long getIdUbicacion() {
+		return idUbicacion;
+	}
+
+	public void setIdUbicacion(Long idUbicacion) {
+		this.idUbicacion = idUbicacion;
+	}
+
+	public Long getIdArticulo() {
+		return idArticulo;
+	}
+
+	public void setIdArticulo(Long idArticulo) {
+		this.idArticulo = idArticulo;
+	}
+
+	public Long getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Long cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getCreadoPor() {
+		return creadoPor;
+	}
+
+	public void setCreadoPor(String creadoPor) {
+		this.creadoPor = creadoPor;
+	}
+
+	public String getActualizadoPor() {
+		return actualizadoPor;
+	}
+
+	public void setActualizadoPor(String actualizadoPor) {
+		this.actualizadoPor = actualizadoPor;
+	}
+
+	public String getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(String fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getUltimaFechaModificacion() {
+		return ultimaFechaModificacion;
+	}
+
+	public void setUltimaFechaModificacion(String ultimaFechaModificacion) {
+		this.ultimaFechaModificacion = ultimaFechaModificacion;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+
+}
