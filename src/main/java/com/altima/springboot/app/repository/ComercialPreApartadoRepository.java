ac1edfab07bf7e014aab7473f300acd62599bdd2
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-11-26 14:25:50 -0600

    Add files via upload

---
+++ src/main/java/com/altima/springboot/app/repository/ComercialPreApartadoRepository.java
@@ -0,0 +1,9 @@
+package com.altima.springboot.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.altima.springboot.app.models.entity.ComercialPreApartado;
+
+public interface ComercialPreApartadoRepository extends CrudRepository<ComercialPreApartado, Long>{
+
+}
