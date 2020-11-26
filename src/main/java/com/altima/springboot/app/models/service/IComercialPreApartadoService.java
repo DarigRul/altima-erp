bec4f1ef5af1d4006e7501b9b5aeec40bcb20bce
victorgarc1a <47542035+victorgarc1a@users.noreply.github.com>
2020-11-26 14:25:08 -0600

    Add files via upload

---
+++ src/main/java/com/altima/springboot/app/models/service/IComercialPreApartadoService.java
@@ -0,0 +1,33 @@
+package com.altima.springboot.app.models.service;
+
+import java.util.List;
+
+import com.altima.springboot.app.models.entity.ComercialCoordinadoPreapartado;
+import com.altima.springboot.app.models.entity.ComercialPreApartado;
+import com.altima.springboot.app.models.entity.ComercialPrendaPreapartado;
+
+public interface IComercialPreApartadoService {
+
+	void save (ComercialPreApartado comercialPreApartado);
+	
+	List<Object[]> findPreapartados ();
+	
+	ComercialPreApartado findOne (Long id);
+	
+	List<ComercialCoordinadoPreapartado> findCoordinadosByPreapartado(Long id);
+	
+	void saveCoordinado(ComercialCoordinadoPreapartado comercialCoordinadoPreapartado);
+	
+	ComercialCoordinadoPreapartado findCoordinado(Long id);
+	
+	void deleteCoordinado(Long id);
+	
+	List<ComercialPrendaPreapartado> findPrendasByCoordinados(Long id);
+	
+	void savePrendaCoordinado(ComercialPrendaPreapartado comercialPrendaPreapartado);
+	
+	ComercialPrendaPreapartado findPrendaCoordinado(Long id);
+	
+	void deletePrendaCoordinado(Long id);
+	
+}
