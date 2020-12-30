c29957b4a308f19e62983d9dc20d809d01d5e522
TIErik <zemp.erik@gmail.com>
2020-12-29 19:12:12 -0600

    roooles de canelita

--- src/main/java/com/altima/springboot/app/controller/TiempoCorteController.java
+++ src/main/java/com/altima/springboot/app/controller/TiempoCorteController.java
@@ -2,7 +2,7 @@ package com.altima.springboot.app.controller;
 
 
 import com.altima.springboot.app.models.service.ITiempoCorteService;
-
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +13,7 @@ public class TiempoCorteController {
     @Autowired
     private ITiempoCorteService TiempoService;
 
+    @Secured({"ROLE_ADMINISTRADOR","ROLE_PRODUCCION_TIEMPO_CORTE"})
     @GetMapping("/tiempos-de-corte")
 	public String tiemposCorte(Model model) {
 
--- src/main/java/com/altima/springboot/app/controller/TiempoCorteRestController.java
+++ src/main/java/com/altima/springboot/app/controller/TiempoCorteRestController.java
@@ -15,7 +15,7 @@ import com.altima.springboot.app.models.entity.ComercialCoordinadoPrenda;
 import com.altima.springboot.app.models.service.IComercialCoordinadoService;
 import com.altima.springboot.app.models.service.IEmpalmeTelasService;
 import com.altima.springboot.app.models.service.ITiempoCorteService;
-
+import org.springframework.security.access.annotation.Secured;
 @RestController
 public class TiempoCorteRestController {
     @Autowired
@@ -24,11 +24,13 @@ public class TiempoCorteRestController {
 	@Autowired
     private IComercialCoordinadoService coorService;
 
+    @Secured({"ROLE_ADMINISTRADOR","ROLE_PRODUCCION_TIEMPO_CORTE_AÑADIR_TIEMPO"})
     @RequestMapping(value="/listar_coordinado_prenda_por_folio", method=RequestMethod.GET)
 	public List<Object []> detalles (@RequestParam(name = "folio") String folio ){
 		return TiempoService.detallesFolio(folio);
     }
     
+    @Secured({"ROLE_ADMINISTRADOR","ROLE_PRODUCCION_TIEMPO_CORTE_AÑADIR_TIEMPO"})
     @RequestMapping(value="/add_tiempo_coordinado_prenda", method=RequestMethod.GET)
 	public boolean add (@RequestParam(name = "id") Long id, @RequestParam(name = "tiempo") String tiempo  ){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
