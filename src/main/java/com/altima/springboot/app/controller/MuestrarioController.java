ecf2eadf8e7dceb1c8af309887a012abd7cad2c4
warriorx72 <39719764+warriorx72@users.noreply.github.com>
2021-02-18 15:48:25 -0600

    cambios para el muestarrio imagenes

--- src/main/java/com/altima/springboot/app/controller/MuestrarioController.java
+++ src/main/java/com/altima/springboot/app/controller/MuestrarioController.java
@@ -26,63 +26,70 @@ import com.altima.springboot.app.models.service.IComercialImagenInventarioServic
 import com.altima.springboot.app.models.service.IInventarioService;
 import com.altima.springboot.app.models.service.IProduccionDetalleService;
 import com.altima.springboot.app.models.service.IUploadService;
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 
 @Controller
 public class MuestrarioController {
-	
+
 	@Autowired
 	private IInventarioService inventario;
-	
+
 	@Autowired
 	private IUploadService UploadService;
-	
+
 	@Autowired
 	private IProduccionDetalleService detallePedidoService;
-	
+
 	@Autowired
 	private IComercialImagenInventarioService serviceImgInventario;
-	
-	@Secured({"ROLE_ADMINISTRADOR", "ROLE_COMERCIAL_MUESTRARIO_INVENTARIO_LISTAR"})
+
+	@Secured({ "ROLE_ADMINISTRADOR", "ROLE_COMERCIAL_MUESTRARIO_INVENTARIO_LISTAR" })
 	@GetMapping("/inventario")
 	public String Inventario(Model model) {
-		
-		List<ProduccionDetallePedido> listInventario= inventario.listInventario();
+
+		List<ProduccionDetallePedido> listInventario = inventario.listInventario();
 		model.addAttribute("listInventario", listInventario);
-		System.out.println("Si esta entrando al controller");	
+		System.out.println("Si esta entrando al controller");
 		return "inventario";
 	}
-	
-	@Secured({"ROLE_ADMINISTRADOR", "ROLE_COMERCIAL_MUESTRARIO_INVENTARIO_EDITAR"})
+
+	@Secured({ "ROLE_ADMINISTRADOR", "ROLE_COMERCIAL_MUESTRARIO_INVENTARIO_EDITAR" })
 	@PostMapping("/find-edit/img")
-	public String findEdit( Model model, @RequestParam(name = "idPrenda", required = false) Long idPrenda,
+	public String findEdit(Model model, @RequestParam(name = "idPrenda", required = false) Long idPrenda,
 			@RequestParam(name = "idTela", required = false) Long idTela,
-			@RequestParam(value="imagen", required=false) MultipartFile imagenInventario) {
-		
-		
+			@RequestParam(value = "imagen", required = false) MultipartFile imagenInventario) throws IOException {
+
+		Cloudinary cloudinary = UploadService.CloudinaryApi();
+
 		System.out.println("si entro al controller editar img");
-		System.out.println("asi llego el id desde el controller"+ idPrenda );	   	    	 	    		    	    	 	    	    	  
-	    	System.out.println("si entro al ifffffffff");	    		    	    		  	    	
-	    	ComercialmagenInventario prenda =  serviceImgInventario.findByidPrendaAndidTela(idPrenda, idTela);
-	    	   
-			if (!imagenInventario.isEmpty()){
-				if ( prenda.getRutaPrenda() != null && prenda.getRutaPrenda().length() > 0) {
-					UploadService.deleteInventario(prenda.getRutaPrenda());
-				}
-				String uniqueFilename = null;
-				try {
-					uniqueFilename = UploadService.copyInventario(imagenInventario);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				prenda.setRutaPrenda(uniqueFilename); 
+		System.out.println("asi llego el id desde el controller" + idPrenda);
+		System.out.println("si entro al ifffffffff");
+		ComercialmagenInventario prenda = serviceImgInventario.findByidPrendaAndidTela(idPrenda, idTela);
+
+		if (!imagenInventario.isEmpty()) {
+			if (prenda.getRutaPrenda() != null && prenda.getRutaPrenda().length() > 0) {
+				UploadService.deleteInventario(prenda.getRutaPrenda());
+				cloudinary.uploader().destroy("muestrario/" + prenda.getRutaPrenda().substring(0, prenda.getRutaPrenda().length() - 4)
+				, ObjectUtils.asMap("resourceType", "image"));
 			}
-			
-			prenda.setIdPrenda(idPrenda);
-			prenda.setIdTela(idTela);
-			
-			serviceImgInventario.save(prenda);
-		
-	   return "redirect:/inventario";
+			String uniqueFilename = null;
+			try {
+				uniqueFilename = UploadService.copyInventario(imagenInventario);
+				cloudinary.uploader().upload(UploadService.filePrenda(uniqueFilename), ObjectUtils.asMap("public_id",
+				"muestrario/" + uniqueFilename.substring(0, uniqueFilename.length() - 4)));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			prenda.setRutaPrenda(uniqueFilename);
+		}
+
+		prenda.setIdPrenda(idPrenda);
+		prenda.setIdTela(idTela);
+
+		serviceImgInventario.save(prenda);
+
+		return "redirect:/inventario";
 	}
 }
