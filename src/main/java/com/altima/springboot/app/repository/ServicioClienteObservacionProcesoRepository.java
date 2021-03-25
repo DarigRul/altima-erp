4bb0314b37d384c2cf5873584866604be75f3434
TIErik <zemp.erik@gmail.com>
2021-03-25 17:54:38 -0600

    arreglos

---
+++ src/main/java/com/altima/springboot/app/repository/ServicioClienteObservacionProcesoRepository.java
@@ -0,0 +1,8 @@
+package com.altima.springboot.app.repository;
+
+import com.altima.springboot.app.models.entity.ServicioClienteObservacionProceso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServicioClienteObservacionProcesoRepository  extends CrudRepository<ServicioClienteObservacionProceso,Long>{
+    
+}
