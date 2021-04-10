d2d02aceb83f5407e721f71c0ad46b6c0e11f3e5
carlos151195 <eliosaamarocarlos@gmail.com>
2021-04-10 14:15:47 -0500

    cambios

---
+++ src/main/java/com/altima/springboot/app/controller/TallerMaquilaHerramientaUtileriaController.java
@@ -0,0 +1,41 @@
+package com.altima.springboot.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.altima.springboot.app.models.service.IMaquilaAsignacionTicketsService;
+import com.altima.springboot.app.models.service.IMaquilaControlPedidoBultoService;
+import com.altima.springboot.app.models.service.IMaquilaControlPedidoService;
+
+@Controller
+public class TallerMaquilaHerramientaUtileriaController {
+
+	@Autowired
+	IMaquilaControlPedidoService maquilaControlPedidoService;
+	@Autowired
+	IMaquilaControlPedidoBultoService maquilaControlPedidoBultoService;
+	@Autowired
+	IMaquilaAsignacionTicketsService maquilaAsignacionTicketsService;
+	
+	@GetMapping("/herramienta-utileria")
+	public String HerramientaUtileria(Model model) {
+		model.addAttribute("control_pedido", maquilaControlPedidoService.findAllMaquilaControlPedido());
+		model.addAttribute("clientes", maquilaControlPedidoService.findAllCliente());
+		model.addAttribute("prenda_modelo", maquilaControlPedidoService.findAllPrendaModelo());
+	
+		return "herramienta-utileria";
+	}
+	
+	@GetMapping("/entradas-salidas-herramientas-utileria")
+	public String EntradasSalidasHerramientaUtileria(Model model) {
+		model.addAttribute("control_pedido", maquilaControlPedidoService.findAllMaquilaControlPedido());
+		model.addAttribute("clientes", maquilaControlPedidoService.findAllCliente());
+		model.addAttribute("prenda_modelo", maquilaControlPedidoService.findAllPrendaModelo());
+	
+		return "entradas-salidas-herramientas-utileria";
+	}
+	
+	
+}
